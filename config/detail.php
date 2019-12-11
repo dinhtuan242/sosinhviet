e174e154ec4826e79893a4ba02502735879d264d
tuanvd <tuanvd@hblab.vn>
2019-12-11 16:15:05 +0700

    Refactor SEO

--- config/detail.php
+++ config/detail.php
@@ -4,4 +4,5 @@
         'author' => env('APP_AUTHOR', 'Vũ Đình Tuân'),
         'keywords' => env('APP_KEYWORDS', 'Sơ Sinh Việt, Hàng trẻ em, Đồ trẻ em, Sữa cho trẻ sơ sinh, Sơ sinh'),
         'facebook_link' => env('FACEBOOK_LINK', 'https://www.facebook.com/sosinhviet/'),
+        'description' => env('APP_DESCRIPTION'),
     ];
---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,20 @@
+@extends('frontend.layouts.app')
+@section('content')
+    <div class="body-content outer-top-bd">
+        <div class="container">
+            <div class="x-page inner-bottom-sm">
+                <div class="row">
+                    <div class="col-md-12 x-text text-center">
+                        <h1>429</h1>
+                        <p>Xin lỗi, hệ thống đang quá tải! </p>
+                        <form role="form" class="outer-top-vs outer-bottom-xs">
+                            <input placeholder="Tìm kiếm sản phẩm"" autocomplete="off">
+                            <button class="btn-default le-button">Tìm kiếm</button>
+                        </form>
+                        <a href="{{ route('home-page') }}"><i class="fa fa-home"></i> Trở về trang chủ</a>
+                    </div>
+                </div><!-- /.row -->
+            </div><!-- /.sigin-in-->
+        </div><!-- /.container -->
+    </div><!-- /.body-content -->
+@endsection
